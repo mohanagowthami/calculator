8e691852ccffbd417fc22ddad866d9eadb02fe7b
ib_admin <sirivalli139@gmail.com>
2019-08-10 12:27:59 +0530

    green state of call back function with number

--- src/components/KeyPad/index.js
+++ src/components/KeyPad/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
 export default class KeyPad extends Component {
+  submit = e => {
+    this.props.onSubmit(e.target.value);
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <button value="1" onClick={this.submit} data-testid="1">
+          1{" "}
+        </button>
+      </div>
+    );
   }
 }
