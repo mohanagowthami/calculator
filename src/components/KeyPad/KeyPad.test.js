820589341a0f29b8a499641f9e5f1bff06d07a8f
ib_admin <sirivalli139@gmail.com>
2019-08-10 12:40:07 +0530

    after completion of refactoring and writinng test cases for every button

--- src/components/KeyPad/KeyPad.test.js
+++ src/components/KeyPad/KeyPad.test.js
@@ -6,8 +6,25 @@ const myMock = jest.fn();
 afterEach(cleanup);
 describe(" should test the callback function with particular value", () => {
   it(" should test call back with particular number ", () => {
-    const { getByTestId } = render(<KeyPad onSubmit={myMock} />);
+    const { getByTestId } = render(<KeyPad onClickNumber={myMock} />);
+
     fireEvent.click(getByTestId("1"));
     expect(myMock).toHaveBeenCalledWith("1");
+    fireEvent.click(getByTestId("2"));
+    expect(myMock).toHaveBeenCalledWith("2");
+    fireEvent.click(getByTestId("3"));
+    expect(myMock).toHaveBeenCalledWith("3");
+    fireEvent.click(getByTestId("4"));
+    expect(myMock).toHaveBeenCalledWith("4");
+    fireEvent.click(getByTestId("5"));
+    expect(myMock).toHaveBeenCalledWith("5");
+    fireEvent.click(getByTestId("6"));
+    expect(myMock).toHaveBeenCalledWith("6");
+    fireEvent.click(getByTestId("7"));
+    expect(myMock).toHaveBeenCalledWith("7");
+    fireEvent.click(getByTestId("8"));
+    expect(myMock).toHaveBeenCalledWith("8");
+    fireEvent.click(getByTestId("9"));
+    expect(myMock).toHaveBeenCalledWith("9");
   });
 });
--- src/components/KeyPad/index.js
+++ src/components/KeyPad/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 
 export default class KeyPad extends Component {
-  submit = e => {
-    this.props.onSubmit(e.target.value);
+  onClickNumber = e => {
+    this.props.onClickNumber(e.target.value);
   };
 
   render() {
     return (
       <div>
-        <button value="1" onClick={this.submit} data-testid="1">
-          1{" "}
+        <button value="1" onClick={this.onClickNumber} data-testid="1">
+          1
+        </button>
+        <button value="2" onClick={this.onClickNumber} data-testid="2">
+          2
+        </button>
+        <button value="3" onClick={this.onClickNumber} data-testid="3">
+          3
+        </button>
+        <button value="4" onClick={this.onClickNumber} data-testid="4">
+          4
+        </button>
+        <button value="5" onClick={this.onClickNumber} data-testid="5">
+          5
+        </button>
+
+        <button value="6" onClick={this.onClickNumber} data-testid="6">
+          6
+        </button>
+        <button value="7" onClick={this.onClickNumber} data-testid="7">
+          7
+        </button>
+        <button value="8" onClick={this.onClickNumber} data-testid="8">
+          8
+        </button>
+        <button value="9" onClick={this.onClickNumber} data-testid="9">
+          9
         </button>
       </div>
     );
