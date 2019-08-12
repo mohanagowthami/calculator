f0d363d5a14c937d5e5ebf7d90130f0fa3e2493e
ib_admin <sirivalli139@gmail.com>
2019-08-12 16:08:49 +0530

    red state of equals operation in integration function

--- src/components/CalculatorContainer/CalculatorContainer.test.js
+++ src/components/CalculatorContainer/CalculatorContainer.test.js
@@ -2,22 +2,18 @@ import CalculatorContainer from "./index";
 import React from "react";
 
 import { render, fireEvent, cleanup } from "@testing-library/react";
+afterEach(cleanup);
 describe(" testing the integration of keypad and screen", () => {
   it("should test the callback,rendering the same value", () => {
-    const { getByText, getByTestId } = render(<CalculatorContainer />);
-
+    const { getByTestId } = render(<CalculatorContainer />);
     fireEvent.click(getByTestId("1"));
-
     fireEvent.click(getByTestId("+"));
-    console.log(getByTestId("input"));
     expect(getByTestId("input").innerHTML).toBe("1+");
   });
 
   it("should test the callback,functionality of clear method", () => {
     const { getByText, getByTestId } = render(<CalculatorContainer />);
-
     fireEvent.click(getByText("C"));
-
     expect(getByTestId("input").innerHTML).toBe("");
   });
 
@@ -28,5 +24,15 @@ describe(" testing the integration of keypad and screen", () => {
     expect(getByTestId("input").innerHTML).toBe("1+");
     fireEvent.click(getByText("Del"));
     expect(getByTestId("input").innerHTML).toBe("1");
+  });
+  it("should test the callback,functionality of equals to method", () => {
+    const { getByText, getByTestId } = render(<CalculatorContainer />);
+    fireEvent.click(getByTestId("1"));
+    fireEvent.click(getByTestId("+"));
+    fireEvent.click(getByTestId("1"));
+    expect(getByTestId("input").innerHTML).toBe("1+1");
+    fireEvent.click(getByText("="));
+    expect(getByTestId("input").innerHTML).toBe("2");
+    expect(getByTestId("previousInput").innerHTML).toBe("1+1");
   });
 });
